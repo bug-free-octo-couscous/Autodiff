fd7c4c4ccd2dfb196e4afa49bfb20fb1fd245b60
jihoo <jihoop0620@gmail.com>
2026-05-12 14:29:20 +0900

    update

--- autodiff.md
+++ autodiff.md
@@ -7,3 +7,10 @@ Computational Graph
 Vector-Jacobian Product
 
 Backpropagation
+
+dual numbers
+$a + b\epsilon$
+
+$\epsilon^2=0$
+
+$$f(x + \epsilon) = f(x) + f'(x)\epsilon + \frac{f''(x)}{2!}\epsilon^2 + \dots = f(x) + f'(x)\epsilon$$
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,34 @@
-module Main where
+data Dual = Dual Double Double deriving (Show, Eq)
 
+instance Num Dual where
+    -- (u + u'ε) + (v + v'ε) = (u+v) + (u'+v')ε
+    (Dual u u') + (Dual v v') = Dual (u + v) (u' + v')
+    
+    -- (u + u'ε) * (v + v'ε) = uv + (uv' + u'v)ε  <- 곱의 미분법!
+    (Dual u u') * (Dual v v') = Dual (u * v) (u * v' + u' * v)
+    
+    fromInteger n = Dual (fromInteger n) 0
+    abs (Dual u u') = Dual (abs u) (u' * signum u)
+    signum (Dual u _) = Dual (signum u) 0
+    negate (Dual u u') = Dual (negate u) (negate u')
+
+instance Fractional Dual where
+    -- 몫의 미분법
+    (Dual u u') / (Dual v v') = Dual (u / v) ((u' * v - u * v') / (v * v))
+
+instance Floating Dual where
+    -- sin(u + u'ε) = sin(u) + u'cos(u)ε
+    sin (Dual u u') = Dual (sin u) (u' * cos u)
+    cos (Dual u u') = Dual (cos u) (-u' * sin u)
+    exp (Dual u u') = Dual (exp u) (u' * exp u)
+
+    -- f(x) = x^2 + sin(x)
+f :: Dual -> Dual
+f x = x * x + sin x
+
+-- x = 2에서의 값과 미분값 구하기
+-- f(2) = 4 + sin(2) ≒ 4.909
+-- f'(2) = 2*2 + cos(2) ≒ 4 - 0.416 = 3.584
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = print $ f (Dual 2 1)
+-- 출력 결과: Dual 4.909297426825682 3.5838531634528574
